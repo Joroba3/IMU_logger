2a7391e5adfd2557910c91337e25fe63fa1a45ac
Jacobo Pindado Perea <pindado.jacobo@gmail.com>
2022-11-07 01:44:49 +0100

    roslaunch

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jacobo/Documents/IMU_logger/src
+# Build directory: /home/jacobo/Documents/IMU_logger/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
